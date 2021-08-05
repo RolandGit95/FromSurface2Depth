1d7242d2e4b3c054284efdbc87ecd4c3d7b58e9e
RolandGit95 <rolandstenger95@gmail.com>
2021-08-05 16:27:50 +0200

    updated

---
+++ scripts/plots/characteristic_variables.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  5 16:22:43 2021
+
+@author: roland
+"""
+
---
+++ scripts/plots/plot_longterm.py
@@ -0,0 +1,50 @@
+# %%
+import numpy as np
+import torch
+
+
+import matplotlib.pyplot as plt
+
+import matplotlib as mpl
+
+import tikzplotlib
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('Found', torch.cuda.device_count(), 'GPUs')
+
+# %%
+fileLongtermA ='/home/roland/Projekte/FromSurface2Depth/data/visualization/regimeA/longterm/regimeA_longterm.npy'
+fileLongtermB ='/home/roland/Projekte/FromSurface2Depth/data/visualization/regimeB/longterm/regimeB_longterm.npy'
+
+# %%
+data = np.load(fileLongtermA)
+voxel_timelineA = (data[:,80,100,0].astype(np.float32)+127)/255.
+
+data = np.load(fileLongtermB)
+voxel_timelineB = (data[:,80,100,0].astype(np.float32)+127)/255.
+
+# %%
+
+mpl.rcParams['mathtext.fontset'] = 'stix'
+mpl.rcParams['font.family'] = 'STIXGeneral'
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(4,2), sharey=True)
+
+plt.subplots_adjust(left=0.0, right=1.0, top=0.95, bottom=0.01)
+
+ax1.plot(voxel_timelineA)
+ax1.set_ylabel(r'$u$')
+ax1.set_xlabel('Time step in $\Delta t$')
+ax1.set_title('Regime A')
+
+plt.sca(ax1)
+plt.xticks(np.linspace(0,512,5), np.linspace(0,512*16,5).astype(int))
+
+ax2.plot(voxel_timelineB)
+ax2.set_xlabel('Time step in $\Delta t$')
+ax2.set_title('Regime B')
+
+plt.sca(ax2)
+plt.xticks(np.linspace(0,512,5), np.linspace(0,512*16,5).astype(int))
+
+tikzplotlib.save('longterms_voxel.tex')
